08fc192027980a8d2d356b5a82deaf68474faaba
BryOk-droid <brianokoth534@gmail.com>
2025-03-04 11:35:57 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = "Scuber";  // Declared as a constant
+
+let mostProfitableNeighborhood = "Chelsea";  // Declared using let
+
+let companyCeo = "Susan Smith";  // Declared using let
